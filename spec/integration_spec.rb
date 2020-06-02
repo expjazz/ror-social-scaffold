6c8987bf8bfa07dbe62ce7cae33b1d5725620275
Expedito Andrade <expeditojazz@gmail.com>
2020-06-02 11:23:04 -0300

    Added last test_case

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -51,6 +51,8 @@ RSpec.describe 'Testing the login', type: :system do
     end
 
     scenario 'accept request' do
+      user3 = FactoryBot.create(:user)
+      friendship = Friendship.create(active: user3, passive: friend, status: false)
       visit root_path
       fill_in 'Email', with: friend.email
       fill_in 'Password', with: 'foobar'
