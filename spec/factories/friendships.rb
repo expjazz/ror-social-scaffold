c87e63c81a84c8c9ea94057e48c8638874c28120
Expedito Andrade <expeditojazz@gmail.com>
2020-06-02 06:42:09 -0300

    re-started rspec

--- spec/factories/friendships.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :friendship do
-    active_id { 1 }
-    passive_id { 1 }
-    status { false }
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-require 'faker'
-FactoryBot.define do
-  factory :user do
-    name { Faker::Name.unique }
-    password { 'foobar' }
-    sequence(:email) { |n| "#{n}#{Faker::Internet.email}" }
-  end
-end
