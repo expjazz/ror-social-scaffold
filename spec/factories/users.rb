f3eb3fd0ef637c67f725a082d377c10c592269e9
Expedito Andrade <expeditojazz@gmail.com>
2020-06-02 11:27:39 -0300

    final tests adjustements

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 require 'faker'
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.name }
+    name { Faker::Name.name[1..15] }
     password { 'foobar' }
     sequence(:email) { |n| "#{n}#{Faker::Internet.email}" }
   end
